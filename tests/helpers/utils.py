89f064849f6366e6fbfc4f83cbba58e14a194e66
gonzygonz <gonzygonz@gmail.com>
2020-02-22 16:51:38 +0200

    Add basic tests for encdec_manager

--- tests/helpers/utils.py
+++ tests/helpers/utils.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import filecmp
 
 ### Helper functions ###
 def create_temp_workspace(path, temp_dir):
@@ -27,3 +28,48 @@ def lock_folder(path):
 def unlock_folder(temp_dir_locker):
     __tracebackhide__ = True
     os.close(temp_dir_locker)
+
+
+def list_files_levels(path: str):
+    __tracebackhide__ = True
+    allFiles = {}
+    allFolders = {}
+    level = 0
+    for root, subfiles, files in os.walk(path, topdown=False):
+        for name in files:
+            allFiles[name, level] = os.path.join(root, name)
+
+        for folder in subfiles:
+            allFolders[folder, level] = os.path.join(root, folder)
+        level +=1
+    return allFiles, allFolders
+
+
+def count_dir_trees_equal(dir1, dir2):
+    __tracebackhide__ = True
+    equal = 0
+    non_equal = 0
+    semi_equal = 0
+    allFiles1, allFolders1 = list_files_levels(dir1)
+    allFiles2, allFolders2 = list_files_levels(dir2)
+
+    keys_files1 = set(allFiles1.keys())
+    keys_files2 = set(allFiles2.keys())
+    keys_folder1 = set(allFolders1.keys())
+    keys_folder2 = set(allFolders2.keys())
+
+    non_equal += len(keys_folder1 - keys_folder2)
+    non_equal += len(keys_folder2 - keys_folder1)
+
+    equal += len(keys_folder1 & keys_folder2)
+
+    non_equal += len(keys_files1 - keys_files2)
+    non_equal += len(keys_files2 - keys_files1)
+
+    for key in keys_files1 & keys_files2:
+        if filecmp.cmp(allFiles1[key], allFiles2[key]):
+            equal += 1
+        else:
+            semi_equal += 1
+
+    return equal, semi_equal, non_equal
--- tests/test_encdecmanager.py
+++ tests/test_encdecmanager.py
@@ -1,8 +1,10 @@
 import random
 import string
+import os
 import pytest
 from enc_files import enc_manager as man
 import tests.helpers.utils as ut
+import filecmp
 
 
 tests_params = [("./tests/inputs/folder_test1", 1),
@@ -18,10 +20,29 @@ def get_new_manager(workers=1):
 
 ### Test functions ###
 @pytest.mark.parametrize("path,workers", tests_params)
-def test_manager_single_file(path, workers, tmpdir):
-    file_to_test = ut.create_temp_workspace(path, tmpdir)
+def test_manager_multi_file(path, workers, tmpdir):
+    work_dir = os.path.join(tmpdir,"work")
+    ref_dir = os.path.join(tmpdir,"ref")
+    file_to_test = ut.create_temp_workspace(path, work_dir)
+    ref_path = ut.create_temp_workspace(path, ref_dir)
+    errors = []
     manager = get_new_manager(workers)
     manager.scan_path(file_to_test)
 
+    # enc files then dec and compare, with/wihtout remove
+    manager.enc_files(remove_old=True)
+    equal, semi_equal, non_equal = ut.count_dir_trees_equal(file_to_test, ref_path)
+    if equal:
+        errors.append(f"Encrypted folder came back identical to original: {equal}")
+    if semi_equal:
+        errors.append(f"Encrypted folder came back files with same name to original: {semi_equal}")
 
+    manager.dec_files(remove_old=True)
+    equal, semi_equal, non_equal = ut.count_dir_trees_equal(file_to_test, ref_path)
+    if non_equal:
+        errors.append(f"Encrypted and then decrypted folder came back  not identical to original: {non_equal}")
+    if semi_equal:
+        errors.append(f"Encrypted folder came back files with same name to original encryption: {semi_equal}")
 
+    # Summarize errors
+    assert not errors, f"errors occurred:\nfile: {file_to_test}\n{chr(10).join(errors)}"
